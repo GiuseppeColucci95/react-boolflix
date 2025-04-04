1e4176d1a1b41428370fabc3becdebcf42faf643
GiuseppeColucci95 <colucci539@gmail.com>
2025-04-04 17:45:01 +0200

    edit milestone 2
    -update movie series labels for consistency in MoviesPage component

--- src/components/MoviesPage.jsx
+++ src/components/MoviesPage.jsx
@@ -62,8 +62,8 @@ export default function MoviesPage() {
               series.map(serie => (
                 <div key={serie.id} className="card mb-2 p-2">
                   <ul className="m-0 p-0 list-unstyled">
-                    <li>{`Titolo: ${serie.name}`}</li>
-                    <li>{`Titolo originale: ${serie.original_name}`}</li>
+                    <li>{`Nome: ${serie.name}`}</li>
+                    <li>{`Nome originale: ${serie.original_name}`}</li>
                     <li>{`Lingua: `}<span className={`fi fi-${serie.original_language}`}></span></li>
                     <li>{`Voto: ${serie.vote_average}`}</li>
                   </ul>
